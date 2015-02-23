1853817a41711f13434030585865fc552fe0178b
Roy D <r.a.david@student.rug.nl>
2015-02-23 12:37:28 +0100

    changed country.py

--- country.py
+++ country.py
@@ -1,63 +1,13 @@
 #!/usr/bin/env python
-#Youri kon niets committen omdat zijn computer het niet deed of niet herkende
-#dus we hebben het samen vanaf mijn pc gedaan
 
-import sys
-from PyQt4 import QtCore, QtGui
-from flag_color.py import FlagColor
+from flag_color import *
 
 class Country():
-	
-	def __init__(self, country):
-		self.country = country
-		self.initUI
+
+		def __init__(self, country):
+			self.country = country
 		
-	def initUI(self):
-		self.combo = QtGui.QComboBox(self)
-		self.combo.addItems(self.getCountryList())
-		self.combo.move(100,50)
-		  
-		self.greet = QtGui.QLabel(self)
-		self.greet.resize(400,50)
-		self.greet.move(110,100)
-		  
-		self.combo.currentIndexChanged.connect(self.updateUI)
+		def __str__(self):
+			return "Hello from {}".format(self.country)
+
 		
-		self.frameobj = QtGui.QFrame(self)
-		self.redobj = QtGui.QColor(0,0,0)
-		self.frameobj.setStyleSheet("QFrame { background-color: %s }" % self.redobj.name())
-		self.redobj = flag_color.FlagColor.chooseColor(self.redobj)
-		  
-		self.frameobj = QtGui.QFrame(self)
-		self.greenobj = QtGui.QColor(0,0,0)
-		self.frameobj.setStyleSheet("QFrame { background-color: %s }" % self.greenobj.name())
-		self.greenobj = flag_color.FlagColor.chooseColor(self.greenobj)
-		  
-		self.frameobj = QtGui.QFrame(self)
-		self.blueobj = QtGui.QColor(0,0,0)
-		self.frameobj.setStyleSheet("QFrame { background-color: %s }" % self.blueobj.name())
-		self.blueobj = flag_color.FlagColor.chooseColor(self.blueobj)
-		  
-	def getcountrylist():
-		countrylist = []
-		with open ('countries_list.txt', 'r') as in_f:
-			for line in in_f:
-				linelist = line.split()
-				countrylist = countrylist + linelist
-		return(countrylist)
-		  
-	def updateUI(self):
-		self.country = self.combo.currentText()
-		self.greet.setText(self.__str__())
-		sttext = "QFrame { background-color: %s }" % flag_color.FlagColor(self.country)
-		print(sttext)
-		self.setStyleSheet(sttext)
-		
-	def __str__(self):
-		return "Hello from {0}".format(self.country)
-  
-if __name__ == "__main__":
-	app = QtGui.QApplication(sys.argv)
-	country = Country()
-	country.show()
-	app.exec_()
