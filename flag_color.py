e510da20f8790e3c77a80f96e9aec8f83b9d5d74
Roy D <r.a.david@student.rug.nl>
2015-02-23 08:26:18 +0100

    Created flag_color

--- flag_color.py
+++ flag_color.py
@@ -1,1 +1,17 @@
-#!/usr/bin/ env python
+#!/usr/bin/env python
+
+from PyQt import QtCore, QtGui
+from random import randrange
+
+class FlagColor(QtGui.QColor):
+	
+	def __init__(self, country):
+		super(FlagColor, self).__init__()
+		self.initUI
+		
+	def initUI(self):
+		self.country = country
+		self.color = self.chooseColor()
+		
+	def chooseColor(self):
+		return(randrange(self.setRed(0, 255)), randrange(self.setGreen(0, 255)), randrange(self.setBlue(0, 255)))
