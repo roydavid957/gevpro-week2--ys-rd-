26df9e46d8db7f2fcebffaa68fdaa80a8b537567
Roy D <r.a.david@student.rug.nl>
2015-02-23 12:33:09 +0100

    added a main

--- flag_color.py
+++ flag_color.py
@@ -1,17 +1,19 @@
-#!/usr/bin/env python
+#!/usr/bin/python3
 
-from PyQt import QtCore, QtGui
+import sys
+from PyQt4 import QtCore, QtGui
 from random import randrange
 
 class FlagColor(QtGui.QColor):
 	
 	def __init__(self, country):
 		super(FlagColor, self).__init__()
-		self.initUI
-		
-	def initUI(self):
-		self.country = country
-		self.color = self.chooseColor()
 		
 	def chooseColor(self):
 		return(randrange(self.setRed(0, 255)), randrange(self.setGreen(0, 255)), randrange(self.setBlue(0, 255)))
+	
+if __name__ == '__main__':
+	app = QApplication(sys.argv)
+	flag_color = FlagColor()
+	flag_color.show()
+	sys.exit(app.exec_())
